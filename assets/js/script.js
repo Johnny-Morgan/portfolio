855b6b09c873dbdc1f9b3372975423cfbc8cf9c4
Johnny-Morgan <jwjmorgan@gmail.com>
2022-01-26 20:56:23 +0000

    Implement dynamic sticky nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
+const homeSection = document.querySelector('#home')
+const nav = document.querySelector('.navbar');
+
+const stickyNav = (entries) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) {
+        nav.classList.add('sticky-top');
+    } else {
+        nav.classList.remove('sticky-top');
+    }
+}
+const homeObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: '-62px',
+});
+homeObserver.observe(homeSection);
+
+// Smooth scrolling
 document.querySelector(".navbar-nav").addEventListener("click", function (e) {
     e.preventDefault();
     if (e.target.classList.contains("nav-link")) {
         const id = e.target.getAttribute("href");
-        console.log(id);
         document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
 });
